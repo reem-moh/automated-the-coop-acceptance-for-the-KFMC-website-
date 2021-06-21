328ee70543eb28146f12a4e62815d3c00e4bc5a7
Norah <381200670@s.mu.edu.sa>
2021-06-21 23:48:22 +0300

    Add contact page

--- KsuTemplate/KsuTemplate/Contact.aspx
+++ KsuTemplate/KsuTemplate/Contact.aspx
@@ -1,17 +1,32 @@
 ﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="KsuTemplate.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your contact page.</h3>
-    <address>
-        One Microsoft Way<br />
-        Redmond, WA 98052-6399<br />
-        <abbr title="Phone">P:</abbr>
-        425.555.0100
-    </address>
+    <link href="CSS/contactStyleSheet1.css" rel="stylesheet" />
 
-    <address>
-        <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
-        <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
-    </address>
+     <body>
+
+
+	<div class="container-contact100">
+		<div class="wrap-contact100">
+                   <h3>
+                       <asp:Label ID="lblOutput" runat="server" ForeColor="#339966"></asp:Label>
+                   </h3>
+                   <p>
+                       &nbsp;</p>
+                   <h2>Get in Touch</h2><br />
+
+					<input class="input100" type="text" name="name" placeholder="Full Name">
+					<input class="input100" type="text" name="email" placeholder="Email" />
+					<textarea class="input100" name="message" placeholder="Your Message"></textarea>
+			       <br />
+                   <asp:Button ID="btnSendEmail" runat="server" BackColor="#2E5882" ForeColor="White" Height="39px" Text="Send Email" Width="139px" OnClick="btnSendEmail_Click" />
+      
+				</div>
+		</div>
+
+
+
+</body>
+
+
 </asp:Content>
--- KsuTemplate/KsuTemplate/Contact.aspx.cs
+++ KsuTemplate/KsuTemplate/Contact.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +14,63 @@ namespace KsuTemplate
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public string sendEmailViaGmail() // worked 100%, this is a nice one use it with  properties
+        {
+            string myFrom = "kfmcintern@gmail.com"; // put your email account (from )
+            string myTo = "kfmcintern@gmail.com"; // put your email account (to )
+            string mySubject = "test sending email ";
+            string myBody = " email message content";
+
+            string myHostsmtpAddress = "smtp.gmail.com";//"smtp.mail.yahoo.com";  //mail.wdbcs.com 
+            int myPortNumber = 587;
+            bool myEnableSSL = true;
+            string myUserName = "kfmcintern@gmail.com";//;
+            string myPassword = "KFMCIntern1234";//;
+
+            //string visitorUserName = Page.User.Identity.Name;
+            using (MailMessage m = new MailMessage(myFrom, myTo, mySubject, myBody)) // .............................1
+            {
+                SmtpClient sc = new SmtpClient(myHostsmtpAddress, myPortNumber); //..................................2
+                try
+                {
+                    sc.Credentials = new System.Net.NetworkCredential(myUserName, myPassword);  //.................3
+                    sc.EnableSsl = true;
+                    sc.Send(m);
+                    return "Email Send successfully";
+                    //lblMsg.Text = ("Email Send successfully");
+                    //lblMsg.ForeColor = Color.Green; // using System.Drawing above 2/2018
+                }
+                catch (SmtpFailedRecipientException ex)
+                {
+                    SmtpStatusCode statusCode = ex.StatusCode;
+                    if (statusCode == SmtpStatusCode.MailboxBusy || statusCode == SmtpStatusCode.MailboxUnavailable || statusCode == SmtpStatusCode.TransactionFailed)
+                    {   // wait 5 seconds, try a second time
+                        Thread.Sleep(5000);
+                        sc.Send(m);
+                        return ex.Message.ToString();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message.ToString();
+                }
+                finally
+                {
+                    m.Dispose();
+                }
+            }// end using 
+        }
+
+
+        protected void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            string msg = sendEmailViaGmail();
+            lblOutput.Text = msg;
         }
     }
 }
