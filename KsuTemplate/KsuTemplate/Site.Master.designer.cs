1a77247d40683d37ab6a1694157ffb0c76228301
Reem <reemalgabbani@gmail.com>
2021-06-30 06:25:53 +0300

    changes

--- KsuTemplate/KsuTemplate/Site.Master.designer.cs
+++ KsuTemplate/KsuTemplate/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace KsuTemplate {
     public partial class SiteMaster {
         
         /// <summary>
+        /// A1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
+        
+        /// <summary>
         /// Image1 control.
         /// </summary>
         /// <remarks>
